8dc9fe8f7542cf74269c1c6e017ed14358289786
LovingtonTech <lovingtontechltd.gmail.com>
2022-04-18 22:22:26 +0300

    router view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
-
 <template>
-	<NavBar />
-	<Footer />
+	<div>
+		<NavBar />
+		<router-view />
+		<Footer />
+	</div>
 </template>
 
 <script setup lang="ts">
-// import NavBar from "./components/NavBar.vue";
-// import Footer from "./components/Footer.vue";
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { createApp } from 'vue'
-import Vue from "vue"
 import App from './App.vue'
 import "virtual:windi.css"
+import router from './router'
 
 //import icons
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { fas } from "@fortawesome/free-solid-svg-icons"
-//import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
 
 
-library.add(fas)
+library.add(fas, fab)
 
 
 
-createApp(App).component("fa", FontAwesomeIcon).mount('#app')
+createApp(App).component("fa", FontAwesomeIcon).use(router).mount('#app')
