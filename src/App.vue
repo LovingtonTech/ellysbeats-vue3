2a2c06d09a8397e12c9d2a7e5a3e6dc7726b5e77
LovingtonTech <lovingtontechltd@gmail.com>
2022-04-19 10:56:37 +0000

    added vuetify3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-	<div>
-		<NavBar />
-		<router-view />
-		<Footer />
-	</div>
+  <v-app>
+    <v-navigation-drawer app> Drawer </v-navigation-drawer>
+
+    <v-app-bar app> <NavBar /> </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+      Footer
+    </v-footer>
+  </v-app>
 </template>
 
 <script setup lang="ts">
@@ -11,5 +24,4 @@ import NavBar from "./components/NavBar.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
-<style>
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import App from './App.vue'
 import "virtual:windi.css"
 import router from './router'
 
+//Vuetify
+import {createVuetify} from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+const vuetify = createVuetify({
+components,
+directives
+})
+
 //import icons
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
@@ -14,4 +24,4 @@ library.add(fas, fab)
 
 
 
-createApp(App).component("fa", FontAwesomeIcon).use(router).mount('#app')
+createApp(App).component("fa", FontAwesomeIcon).use(router,vuetify).mount('#app')
