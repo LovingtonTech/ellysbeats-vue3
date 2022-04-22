d4486079abd8c9a3f2917484f848d03bc0a69852
LovingtonTech <lovingtontechltd@gmail.com>
2022-04-22 14:48:30 +0300

    routes

---
+++ src/components/Contacts.vue
@@ -0,0 +1,3 @@
+<template>
+<div>Contact us via .</div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import About from "../components/About.vue";
+import Contacts from "../components/Contacts.vue";
+import Login from "../components/Login.vue";
+import Signup from "../components/Signup.vue";
 
 const routes = [
 	{
@@ -10,6 +13,18 @@ const routes = [
 	{
 		path: "/about",
 		component: About,
+	},
+	{
+		path: "/contacts",
+		component: Contacts,
+	},
+	{
+		path: "/login",
+		component: Login,
+	},
+	{
+		path: "/signup",
+		component: Signup,
 	},
 ];
 
