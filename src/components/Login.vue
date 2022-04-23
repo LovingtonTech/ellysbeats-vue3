a274d603840fa4c7c47f30fc012173e6d1944c99
LovingtonTech <lovingtontechltd@gmail.com>
2022-04-23 08:52:00 +0300

    1 file modified, 1 added

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Login</div>
+  <div class="w-full flex justify-center items-center">
+    <form @submit.prevent="" class="form">
+      <h2>Login</h2>
+      <div class="input-field">
+        <label for="email">Email</label>
+        <input type="email" placeholder="Email" id="email" required />
+      </div>
+      <div class="input-field">
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" required />
+      </div>
+      <button class="btn">Login</button>
+    </form>
+  </div>
 </template>
+<style scoped>
+.form {
+  @apply my-20 border-3 shadow-xl rounded-xl px-6 py-8;
+}
+.input-field label {
+  @apply block;
+}
+.input-field input {
+  @apply block p-2 border-b-2;
+  @apply focus:(outline-none border-blue-300);
+}
+.btn {
+  @apply w-full bg-blue-400 rounded-lg py-1 text-white text-bolder my-5;
+}
+</style>
