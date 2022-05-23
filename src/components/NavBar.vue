4dfed8233538f1a40e2c0f60204c95d91875de9d
LovingtonTech <lovingtontechltd@gmail.com>
2022-05-23 10:31:41 +0300

    fixed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -34,68 +34,59 @@
 </template>
 
 <script setup>
-import {} from 'vue';
+import { ref, onMounted } from 'vue';
 
-const dropdown = reef(false);
+//Data
+const dropdown = ref(false);
+
+//Lifecycle
+onMounted(() => {
+  console.log('Mounted');
+  window.addEventListener('resize', resetDropdown);
+  window.addEventListener('scroll', resetDropdown);
+  resetDropdown;
+  const links = document.querySelectorAll('a');
+  links.forEach((link) => link.addEventListener('click', resetDropdown));
+});
+
+//Methods
 const toggleDropdown = () => {
-  dropdown = !dropdown;
+  dropdown.value = !dropdown.value;
+};
+
+const resetDropdown = () => {
+  dropdown.value = false;
+};
+
+const openCart = () => {
+  alert('This is a cart');
+  //console.log('Cart open');
+  //this.$router.push('/login');
 };
 </script>
-
-<!-- // // import { MenuIcon, XIcon, ShoppingCartIcon } from "@heroicons/vue/solid";
-// export default {
-//   // components: { MenuIcon, XIcon, ShoppingCartIcon },
-//   mounted() {
-//     console.log('Mounted');
-//     window.addEventListener('resize', this.resetDropdown);
-//     window.addEventListener('scroll', this.resetDropdown);
-//     this.resetDropdown;
-//     const links = document.querySelectorAll('a');
-//     links.forEach((link) => link.addEventListener('click', this.resetDropdown));
-//   },
-//   data() {
-//     return {
-//       dropdown: false,
-//     };
-//   },
-//   methods: {
-//     resetDropdown() {
-//       this.dropdown = false;
-//     },
-//     toggleDropdown() {
-//       this.dropdown = !this.dropdown;
-//       console.log(this.dropdown);
-//     },
-//     openCart() {
-//       alert('This is a cart');
-//       console.log('Cart open');
-//       this.$router.push('/login');
-//     },
-//   },
-// }; -->
 
 <style scoped>
 .navlinks a {
   @apply hover:(border-b-blue-500  border-3 text-xl);
 }
-/* .navlinks {
-	@apply sm:(flex flex-row px-4 justify-between) hidden;
+.navlinks {
+  @apply sm:(flex flex-row px-4 justify-between) hidden;
 }
 .navlinks a {
-	@apply p-3 hover:bg-blue-400 hover:text-white;
+  @apply p-3 hover:bg-blue-400 hover:text-white;
 }
 a.nuxt-link-exact-active {
-	@apply bg-blue-400 text-white;
+  @apply bg-blue-400 text-white;
 }
 .navtoggle {
-	@apply sm:(hidden);
+  @apply sm:(hidden);
 }
 
 .dropdownNav {
-	@apply flex flex-col h-full left-0 top-0 fixed;
-	@apply bg-blue-300 text-center;
+  @apply flex flex-col h-full left-0 top-0 fixed;
+  @apply bg-blue-300 text-center;
 }
 .dropdownNav a {
-	@apply w-full;
-} */
+  @apply w-full;
+}
 </style>
